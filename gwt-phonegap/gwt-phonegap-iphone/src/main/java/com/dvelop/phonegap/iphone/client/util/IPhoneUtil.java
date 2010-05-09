0a4351055fa96eaa9962ae81959a3ca854427eb1
Daniel Kurka <kurka.daniel@googlemail.com>
2010-05-09 22:09:21 +0000

    added base classes for iphone widgets

---
+++ gwt-phonegap/gwt-phonegap-iphone/src/main/java/com/dvelop/phonegap/iphone/client/util/IPhoneUtil.java
@@ -0,0 +1,24 @@
+package com.dvelop.phonegap.iphone.client.util;
+
+import com.google.gwt.dom.client.Element;
+
+/**
+ * User: daniel kurka
+ * Date: 10.05.2010
+ * Time: 00:06:29
+ */
+public class IPhoneUtil {
+
+    public static native boolean hasTouchEvent(Element e) /*-{
+		var ua = navigator.userAgent.toLowerCase();
+
+		if (ua.indexOf("safari") != -1 && ua.indexOf("mobile") != -1 && ua.indexOf("applewebkit") != -1)
+		{
+			return true;
+		}
+		else
+		{
+			return false;
+		}
+	}-*/;
+}
