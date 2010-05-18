d0ab88776a884f2dbf7ca7b3204a4358c967a770
Daniel Kurka <kurka.daniel@googlemail.com>
2010-05-18 19:11:11 +0000

    added handler registration for all important native events

--- gwt-phonegap/gwt-phonegap-iphone/src/main/java/org/dvelop/phonegap/iphone/client/widgets/base/IPhoneWidget.java
+++ gwt-phonegap/gwt-phonegap-iphone/src/main/java/org/dvelop/phonegap/iphone/client/widgets/base/IPhoneWidget.java
@@ -8,10 +8,11 @@ import com.google.gwt.event.dom.client.ClickHandler;
 import com.google.gwt.event.shared.GwtEvent;
 import com.google.gwt.event.shared.HandlerRegistration;
 import com.google.gwt.user.client.ui.Widget;
-import org.dvelop.phonegap.iphone.client.widgets.event.gesture.GestureChangeEvent;
-import org.dvelop.phonegap.iphone.client.widgets.event.gesture.GestureEndEvent;
-import org.dvelop.phonegap.iphone.client.widgets.event.gesture.GestureStartEvent;
-import org.dvelop.phonegap.iphone.client.widgets.event.gesture.NativeGestureEvent;
+import org.dvelop.phonegap.iphone.client.widgets.event.gesture.*;
+import org.dvelop.phonegap.iphone.client.widgets.event.touch.*;
+import org.dvelop.phonegap.iphone.client.widgets.event.transition.NativeTransitionEvent;
+import org.dvelop.phonegap.iphone.client.widgets.event.transition.TransitionEndEvent;
+import org.dvelop.phonegap.iphone.client.widgets.event.transition.TransitionEndHandler;
 
 /**
  * Created by IntelliJ IDEA.
@@ -27,6 +28,43 @@ public class IPhoneWidget extends Widget {
 
     public IPhoneWidget(String tagName) {
         setElement(Document.get().createElement(tagName));
+    }
+
+    protected HandlerRegistration addTransitionEndHandler(TransitionEndHandler transitionEndHandler) {
+        return addHandler(transitionEndHandler, TransitionEndEvent.getType());
+    }
+
+    protected HandlerRegistration addTouchStartHandler(TouchStartHandler touchStartHandler) {
+        return addHandler(touchStartHandler, TouchStartEvent.getType());
+    }
+
+    protected HandlerRegistration addTouchMoveHandler(TouchMoveHandler touchMoveHandler) {
+        return addHandler(touchMoveHandler, TouchMoveEvent.getType());
+    }
+
+    protected HandlerRegistration addTouchEndHandler(TouchEndHandler touchEndHandler) {
+        return addHandler(touchEndHandler, TouchEndEvent.getType());
+
+    }
+
+    protected HandlerRegistration addTouchCancelHandler(TouchCancelHandler touchCancelHandler) {
+        return addHandler(touchCancelHandler, TouchCancelEvent.getType());
+
+    }
+
+    protected HandlerRegistration addGestureStartHandler(GestureStartHandler gestureStartHandler) {
+        return addHandler(gestureStartHandler, GestureStartEvent.getType());
+
+    }
+
+    protected HandlerRegistration addGestureChangeHandler(GestureChangeHandler gestureChangeHandler) {
+        return addHandler(gestureChangeHandler, GestureChangeEvent.getType());
+
+    }
+
+    protected HandlerRegistration addGestureEndHandler(GestureEndHandler gestureEndHandler) {
+        return addHandler(gestureEndHandler, GestureEndEvent.getType());
+
     }
 
 
@@ -79,47 +117,57 @@ public class IPhoneWidget extends Widget {
         tearDownEventHooks();
     }
 
-    private void onDomTouchStart() {
-        touchMoved = false;
+
+    //
+    //  touch call backs called from jsni
+    //
+
+    private void onDomTouchStart(NativeTouchEvent event) {
+        fireEvent(new TouchStartEvent(event));
     }
 
-    private void onDomTouchMove() {
-        touchMoved = true;
+    private void onDomTouchMove(NativeTouchEvent event) {
+        fireEvent(new TouchMoveEvent(event));
     }
 
-    private void onDomTouchEnd() {
-        if (touchMoved) {
-            return;
-        }
+    private void onDomTouchEnd(NativeTouchEvent event) {
+        fireEvent(new TouchEndEvent(event));
+    }
 
-        fireEvent(new TouchEvent(this));
+    private void onDomTouchCancel(NativeTouchEvent event) {
+        fireEvent(new TouchCancelEvent(event));
     }
 
 
-    private void onDomTransitionEnded() {
-
-    }
-
-    private JavaScriptObject transitionCallbackEndFunction;
-
+    //set up native touch event handlers
 
     private native void registerTouchEvents() /*-{
 		var instance = this;
 		var element = this.@org.dvelop.phonegap.iphone.client.widgets.base.IPhoneWidget::getElement()();
 
 		element.ontouchstart = function(e){
-			instance.@org.dvelop.phonegap.iphone.client.widgets.base.IPhoneWidget::onDomTouchStart()();
+			instance.@org.dvelop.phonegap.iphone.client.widgets.base.IPhoneWidget::onDomTouchStart(Lorg/dvelop/phonegap/iphone/client/widgets/event/touch/NativeTouchEvent;)(e);
 		};
 
 		element.ontouchmove = function(e){
-			instance.@org.dvelop.phonegap.iphone.client.widgets.base.IPhoneWidget::onDomTouchMove()();
+			instance.@org.dvelop.phonegap.iphone.client.widgets.base.IPhoneWidget::onDomTouchMove(Lorg/dvelop/phonegap/iphone/client/widgets/event/touch/NativeTouchEvent;)(e);
 		};
 
 		element.ontouchend = function(e){
-			instance.@org.dvelop.phonegap.iphone.client.widgets.base.IPhoneWidget::onDomTouchEnd()();
+			instance.@org.dvelop.phonegap.iphone.client.widgets.base.IPhoneWidget::onDomTouchEnd(Lorg/dvelop/phonegap/iphone/client/widgets/event/touch/NativeTouchEvent;)(e);
 		};
 
+        element.ontouchcancel = function(e){
+        instance.@org.dvelop.phonegap.iphone.client.widgets.base.IPhoneWidget::onDomTouchCancel(Lorg/dvelop/phonegap/iphone/client/widgets/event/touch/NativeTouchEvent;)(e);
+        };
+
+
+
+
+
 	}-*/;
+
+    //remove native touch events
 
     private native void unregisterTouchEvents() /*-{
 		var instance = this;
@@ -127,44 +175,45 @@ public class IPhoneWidget extends Widget {
 		element.ontouchstart = null;
 		element.ontouchmove = null;
 		element.ontouchend = null;
+        element.ontochcancel = null;
 	}-*/;
+
+
+    private void onDomTransitionEnded(NativeTransitionEvent event) {
+        fireEvent(new TransitionEndEvent(event));
+    }
+
+
+    //transistion events
+
 
     private native void registerTransistionEndDomEvent() /*-{
         var instance = this;
 		var element = this.@org.dvelop.phonegap.iphone.client.widgets.base.IPhoneWidget::getElement()();
+		var callBack = function(e){
+	        instance.@org.dvelop.phonegap.iphone.client.widgets.base.IPhoneWidget::onDomTransitionEnded(Lorg/dvelop/phonegap/iphone/client/widgets/event/transition/NativeTransitionEvent;)(e);
+		};
 
-        try
-		{
-
-
-			var callBack = function(e){
-				instance.@org.dvelop.phonegap.iphone.client.widgets.base.IPhoneWidget::onDomTransitionEnded()();
-			};
-
-			element.addEventListener('webkitTransitionEnd', callBack, false);
-
-            instance.@org.dvelop.phonegap.iphone.client.widgets.base.IPhoneWidget::transitionCallbackEndFunction = callback;
-		}
-		catch (exception)
-		{
-		}
+		element.webkittransitionend = callBack;
     }-*/;
 
     private native void unregisterTransitionEndDomEvent() /*-{
         var instance = this;
 		var element = this.@org.dvelop.phonegap.iphone.client.widgets.base.IPhoneWidget::getElement()();
-        element.removeEventListener('webkitTransitionEnd', instance.@org.dvelop.phonegap.iphone.client.widgets.base.IPhoneWidget::transitionCallbackEndFunction, false);
+        element.webkittransitionend = null;
     }-*/;
 
-    protected void onDomGestureStart(NativeGestureEvent event) {
+    //gesture events
+
+    private void onDomGestureStart(NativeGestureEvent event) {
         fireEvent(new GestureStartEvent(event));
     }
 
-    protected void onDomGestureChange(NativeGestureEvent event) {
+    private void onDomGestureChange(NativeGestureEvent event) {
         fireEvent(new GestureChangeEvent(event));
     }
 
-    protected void onDomGestureEnd(NativeGestureEvent event) {
+    private void onDomGestureEnd(NativeGestureEvent event) {
         fireEvent(new GestureEndEvent(event));
     }
 
