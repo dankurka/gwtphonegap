7ea558f69395a6afe5dee7ca5e6d2788073c55a8
Daniel Kurka <kurka.daniel@googlemail.com>
2010-10-08 22:07:00 +0000

    added javadoc

--- gwt-phonegap/src/main/java/de/kurka/phonegap/client/PhoneGapTimeoutEvent.java
+++ gwt-phonegap/src/main/java/de/kurka/phonegap/client/PhoneGapTimeoutEvent.java
@@ -1,7 +1,27 @@
+/*
+ * Copyright 2010 Daniel Kurka
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ * 
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
 package de.kurka.phonegap.client;
 
 import com.google.gwt.event.shared.GwtEvent;
 
+/**
+ * This event is fired if phonegap was not successfully initialized in a given time period
+ * @author Daniel Kurka
+ *
+ */
 public class PhoneGapTimeoutEvent extends GwtEvent<PhoneGapTimeoutHandler> {
 
 	public static final GwtEvent.Type<PhoneGapTimeoutHandler> TYPE = new Type<PhoneGapTimeoutHandler>();
