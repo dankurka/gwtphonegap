fecfa278f4fb706a35ee9eb44692e90133c5be43
Daniel Kurka <kurt@ncco.de>
2012-01-20 00:08:31 +0100

    FIXES ISSUE 31

--- src/main/java/com/googlecode/gwtphonegap/client/file/FileSystem.java
+++ src/main/java/com/googlecode/gwtphonegap/client/file/FileSystem.java
@@ -1,10 +1,9 @@
 package com.googlecode.gwtphonegap.client.file;
 
 public interface FileSystem {
-	public static final int LocalFileSystem_PERSISTENT = 0;
 
-	public static final int LocalFileSystem_TEMPORARY = 1;
-
+	public static final int LocalFileSystem_TEMPORARY = 0;
+	public static final int LocalFileSystem_PERSISTENT = 1;
 	public static final int LocalFileSystem_RESOURCE = 2;
 	public static final int LocalFileSystem_APPLICATION = 3;
 
